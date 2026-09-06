8f5c499647cf785082cb457d8d7c8276d84bc655
ImDrying <161957529+ImDrying@users.noreply.github.com>
2026-09-06 02:19:48 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,5 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
-import { getAnalytics, isSupported } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-analytics.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyC25MNk0UMhBpkohdopTgMfJsUa_NfdAF8",
@@ -12,9 +11,5 @@ const firebaseConfig = {
   measurementId: "G-WPCDNV2XWH"
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-
-export const analyticsReady = isSupported()
-  .then((supported) => (supported ? getAnalytics(app) : null))
-  .catch(() => null);
